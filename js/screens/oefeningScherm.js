f9f810120dccb41376ab66f09eb9ea37eaefd6b2
jaybird <jayvenco@outlook.com>
2026-09-18 10:24:16 +0200

    feat: voeg oefenachtergronden toe

--- js/screens/oefeningScherm.js
+++ js/screens/oefeningScherm.js
@@ -8,6 +8,18 @@
 import { vindOefening } from "../exercises.js";
 import { getInstellingen } from "../storage.js";
 
+const OPGAVE_ACHTERGRONDEN = {
+  getallenlijn: "/img/opgave-achtergronden/natuur-bos.png?v=1",
+  tafels: "/img/opgave-achtergronden/ruimte.png?v=1",
+  plusmin: "/img/opgave-achtergronden/anime-fantasie.png?v=1",
+  verhaaltjes: "/img/opgave-achtergronden/onderwater.png?v=1",
+  standaard: "/img/opgave-achtergronden/bergen.png?v=1",
+};
+
+function achtergrondVoorOefening(exerciseId) {
+  return OPGAVE_ACHTERGRONDEN[exerciseId] || OPGAVE_ACHTERGRONDEN.standaard;
+}
+
 /**
  * Toont een specifieke oefening.
  * @param {HTMLElement} container
@@ -15,6 +27,8 @@ import { getInstellingen } from "../storage.js";
  */
 export async function toonOefeningScherm(container, exerciseId) {
   container.innerHTML = "";
+  container.classList.add("opgave-pagina");
+  container.style.setProperty("--opgave-achtergrond", `url("${achtergrondVoorOefening(exerciseId)}")`);
   const oefening = vindOefening(exerciseId);
 
   if (!oefening) {
@@ -31,6 +45,7 @@ export async function toonOefeningScherm(container, exerciseId) {
   }
 
   const terugRij = document.createElement("div");
+  terugRij.className = "opgave-pagina__terug";
   terugRij.style.marginBottom = "16px";
   const terugLink = document.createElement("a");
   terugLink.className = "terug-link";
@@ -40,6 +55,7 @@ export async function toonOefeningScherm(container, exerciseId) {
   container.appendChild(terugRij);
 
   const oefenContainer = document.createElement("div");
+  oefenContainer.className = "opgave-pagina__inhoud";
   container.appendChild(oefenContainer);
 
   const settings = (await getInstellingen(oefening.id)) || {};
